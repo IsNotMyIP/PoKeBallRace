7b2a6c8787d0fbeff35fbca3e63308eb61dbb161
IsNotMyIP <SirSeanHD@gmail.com>
2014-03-13 12:34:24 +0100

    added linear damping && changed bouncing

--- src/js/game.js
+++ src/js/game.js
@@ -14,13 +14,15 @@
 
       this.pokeball1 = this.add.sprite(x, y, 'ball');
       this.pokeball1.anchor.setTo(0.5, 0.5);
-      this.pokeball1.body.bounce.setTo(1,1);
+      this.pokeball1.body.bounce.setTo(0.9,0.9);
       this.pokeball1.body.setCircle(17.5,17.5,17.5);
-
+      this.pokeball1.body.linearDamping = 0.8;
+      ////////////////////////////////////////////////
       this.pokeball2 = this.add.sprite(x, y, 'ball');
       this.pokeball2.anchor.setTo(0.5, 0.5);
-      this.pokeball2.body.bounce.setTo(1,1);
+      this.pokeball2.body.bounce.setTo(0.9, 0.9);
       this.pokeball2.body.setCircle(17.5,17.5,17.5);
+      this.pokeball2.body.linearDamping = 0.8;
 
       this.input.onDown.add(this.onInputDown, this);
     },
@@ -42,33 +44,33 @@
       {
         this.pokeball1.body.velocity.x -= 1.5 * this.time.elapsed;
       }
-      else if (this.pokeball1.body.velocity.x < 0) {
-        this.pokeball1.body.velocity.x += 0.75 * this.time.elapsed;
-      }
+     //else if (this.pokeball1.body.velocity.x < 0) {
+       // this.pokeball1.body.velocity.x += 0.75 * this.time.elapsed;
+      //}
       //RAIGT
       if (this.input.keyboard.isDown(Phaser.Keyboard.RIGHT))
       {
         this.pokeball1.body.velocity.x += 1.5 * this.time.elapsed;
       }
-      else if (this.pokeball1.body.velocity.x > 0) {
-        this.pokeball1.body.velocity.x -= 0.75 * this.time.elapsed;
-      }
+      //else if (this.pokeball1.body.velocity.x > 0) {
+        //this.pokeball1.body.velocity.x -= 0.75 * this.time.elapsed;
+      //}
       //EL BUEN UP
       if (this.input.keyboard.isDown(Phaser.Keyboard.UP))
       {
         this.pokeball1.body.velocity.y -= 1.5 * this.time.elapsed;
       }
-      else if (this.pokeball1.body.velocity.y < 0) {
-        this.pokeball1.body.velocity.y += 0.75 * this.time.elapsed;
-      }
+      //else if (this.pokeball1.body.velocity.y < 0) {
+        //this.pokeball1.body.velocity.y += 0.75 * this.time.elapsed;
+      //}
       //EL MAL DAUN
       if (this.input.keyboard.isDown(Phaser.Keyboard.DOWN))
       {
         this.pokeball1.body.velocity.y += 1.5 * this.time.elapsed;
       }
-      else if (this.pokeball1.body.velocity.y > 0) {
-        this.pokeball1.body.velocity.y -= 0.75 * this.time.elapsed;
-      }
+      //else if (this.pokeball1.body.velocity.y > 0) {
+        //this.pokeball1.body.velocity.y -= 0.75 * this.time.elapsed;
+      //}
 
 
       //POKEBOLA NUMERO DOSITO
@@ -77,33 +79,33 @@
       {
         this.pokeball2.body.velocity.x -= 1.5 * this.time.elapsed;
       }
-      else if (this.pokeball2.body.velocity.x < 0) {
-        this.pokeball2.body.velocity.x += 0.75 * this.time.elapsed;
-      }
+      //else if (this.pokeball2.body.velocity.x < 0) {
+        //this.pokeball2.body.velocity.x += 0.75 * this.time.elapsed;
+      //}
       //RAIGT
       if (this.input.keyboard.isDown(Phaser.Keyboard.D))
       {
         this.pokeball2.body.velocity.x += 1.5 * this.time.elapsed;
       }
-      else if (this.pokeball2.body.velocity.x > 0) {
-        this.pokeball2.body.velocity.x -= 0.75 * this.time.elapsed;
-      }
+      //else if (this.pokeball2.body.velocity.x > 0) {
+        //this.pokeball2.body.velocity.x -= 0.75 * this.time.elapsed;
+      //}
       //EL BUEN UP
       if (this.input.keyboard.isDown(Phaser.Keyboard.W))
       {
         this.pokeball2.body.velocity.y -= 1.5 * this.time.elapsed;
       }
-      else if (this.pokeball2.body.velocity.y < 0) {
-        this.pokeball2.body.velocity.y += 0.75 * this.time.elapsed;
-      }
+      //else if (this.pokeball2.body.velocity.y < 0) {
+        //this.pokeball2.body.velocity.y += 0.75 * this.time.elapsed;
+      //}
       //EL MAL DAUN
       if (this.input.keyboard.isDown(Phaser.Keyboard.S))
       {
         this.pokeball2.body.velocity.y += 1.5 * this.time.elapsed;
       }
-      else if (this.pokeball2.body.velocity.y > 0) {
-        this.pokeball2.body.velocity.y -= 0.75 * this.time.elapsed;
-      }
+      //else if (this.pokeball2.body.velocity.y > 0) {
+        //this.pokeball2.body.velocity.y -= 0.75 * this.time.elapsed;
+      //}
 
       this.game.physics.collide(this.pokeball1, this.pokeball2);
     },
