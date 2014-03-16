a7440f478ae6a7e0886b351e51b36d209ddba82f
seven <gtsnaker@gmail.com>
2014-03-16 20:10:20 +0100

    menu

--- src/js/menu.js
+++ src/js/menu.js
@@ -13,15 +13,15 @@
         , y = this.game.height / 2;
 
 
-      this.titleTxt = this.add.bitmapText(x, y, 'Example Game', {font: '16px minecraftia', align: 'center'});
+      this.titleTxt = this.add.bitmapText(x, y, 'Sumo Ball!', {font: '16px minecraftia', align: 'center'});
       this.titleTxt.anchor.setTo(0.5, 0.5);
 
       y = y + this.titleTxt.height + 5;
-      this.startTxt = this.add.bitmapText(x, y, 'START', {font: '12px minecraftia', align: 'center'});
+      this.startTxt = this.add.bitmapText(x, y+50, 'Blue Player: UP, DOWN, LEFT, RIGHT\nPurple Player: W, S, A, D\nPress Enter to Start', {font: '12px minecraftia', align: 'center'});
       this.startTxt.anchor.setTo(0.5, 0.5);
 
       this.input.onDown.add(this.onDown, this);
-      
+
     },
 
     update: function () {
